5eced1e22075917e2582963b491a9fae16b645a7
Steve Sample <stephen.sample@iotashome.com>
2018-09-14 14:12:00 -0700

    adding auth and config properties

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-var Service, Characteristic;
 const request = require('request');
 const url = require('url');
 const axios = require('axios');
 
-const token = "Bearer eyJhbGciOiJFUzI1NiJ9.eyJpYXQiOjE1MzYzNjU3MDQsInN1YiI6IjU5IiwiZXhwIjoxNTM2MzY2NjA0fQ.s0EIPins02-wAtehALzBT9JFcWBedB66h0h30EutNXsiugijZUb185cfhpFkVBN3JoxohZaBWo9ehcESqJYgOw";
-const featureId = "16185";
+var Service, Characteristic;
 
 module.exports = function (homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-light-iotas", "MyAwesomeSwitch", mySwitch);
+  homebridge.registerAccessory("homebridge-light-iotas", "IotasSwitch", iotasSwitch);
 };
 
-mySwitch.prototype = {
+iotasSwitch.prototype = {
+
   getServices: function () {
-    console.log('here!');
+    console.log('getServices');
+    const self = this;
+    
     let informationService = new Service.AccessoryInformation();
     informationService
       .setCharacteristic(Characteristic.Manufacturer, "My switch manufacturer")
       .setCharacteristic(Characteristic.Model, "My switch model")
       .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
  
-    let switchService = new Service.Switch("My switch");
+    let switchService = new Service.Switch(self.accessoryName);
     switchService
       .getCharacteristic(Characteristic.On)
         .on('get', this.getSwitchOnCharacteristic.bind(this))
@@ -29,64 +30,66 @@ mySwitch.prototype = {
  
     this.informationService = informationService;
     this.switchService = switchService;
+
+    console.log('authing');
+
+    authenticate(self.iotasUrl, self.username, self.password).then((response) => {
+      console.log('setting auth token');
+      this.token = response.jwt;
+      this.refreshToken = response.refresh;
+    }).catch((error) => {
+      console.error('error authenticating');
+    });
+
     return [informationService, switchService];
   },
 
   getSwitchOnCharacteristic: function (next) {
-    const me = this;
-    // request({
-    //     url: me.getUrl,
-    //     method: 'GET',
-    // }, 
-    // function (error, response, body) {
-    //   if (error) {
-    //     me.log('STATUS: ' + response.statusCode);
-    //     me.log(error.message);
-    //     return next(error);
-    //   }
-    //   return next(null, body.currentState);
-    // });
-    console.log("query state");
-    // console.log("me.featureId: ");
-    // console.log(me.featureId);
-    getFeature(me.iotasUrl, featureId).then((res) => {
+    const self = this;
+    console.log("request to query state");
+
+    getFeature(self.iotasUrl, self.token, self.featureId).then((res) => {
       let value = res.value === 1;
       console.log('returning a value of: ' + value + ' for feature state');
       return next(null, value);
     }).catch((err) => {
-      me.log('error turning on');
-      me.log(err);
+      self.log('error turning on');
+      self.log(err);
       return next(err);
     });
   },
    
   setSwitchOnCharacteristic: function (on, next) {
-    const me = this;
-    me.log('setting switch value to: ' + on);
+    const self = this;
+    self.log('setting switch value to: ' + on);
     let value = on ? 1 : 0;
-    updateFeature(me.baseUrl, value, featureId).then((res) => {
+    updateFeature(self.iotasUrl, self.token, value, self.featureId).then((res) => {
       return next();
     }).catch((err) => {
-      me.log('error turning on');
-      me.log(err);
+      self.log('error turning on');
+      self.log(err);
       return next(err);
     });
   }
 };
  
-function mySwitch(log, config) {
+function iotasSwitch(log, config) {
   this.log = log;
-  // this.iotasUrl = url.parse(config['iotasUrl']);
+  this.iotasUrl = config.iotasUrl;
+  this.accessoryName = config.name;
+  this.featureId = config.featureId;
+  this.username = config.username;
+  this.password = config.password;
 }
 
-function updateFeature(iotasUrl, value, featureId) {
+function updateFeature(iotasUrl, token, value, featureId) {
   let body = {
     value: value
   }
   let config = {
-    baseURL: "https://staging-api.iotashome.com/v1/api",
+    baseURL: iotasUrl,
     headers: {
-      Authorization: token
+      Authorization: `Bearer ${token}`
     }
   }
   return axios.put(`/feature/${featureId}`, body, config).then((response) => {
@@ -96,16 +99,35 @@ function updateFeature(iotasUrl, value, featureId) {
   });
 }
 
-function getFeature(iotasUrl, featureId) {
+function getFeature(iotasUrl, token, featureId) {
   return axios.get(`/feature/${featureId}`, {
-    baseURL: "https://staging-api.iotashome.com/v1/api",
+    baseURL: iotasUrl,
     headers: {
-      Authorization: token
+      Authorization: `Bearer ${token}`
     }
   }).then((response) => {
     console.log('successful get of feature state');
     return response.data;
   }).catch((error) => {
-    console.log('error: ' + error);
+    console.log('error');
+    console.log(error);
+  });
+}
+
+function authenticate(iotasUrl, username, password) {
+  let config = {
+    baseURL: iotasUrl,
+    auth: {
+      username: username,
+      password: password
+    }
+  }
+  return axios.post(`/auth/tokenwithrefresh`, {}, config).then((response) => {
+    console.log('authentication response');
+    console.log(response);
+    return response.data;
+  }).catch((error) => {
+    console.log('error: ');
+    console.log(error);
   });
 }
